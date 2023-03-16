037f2a7a1f849c4b5374c209ccbef14679f704ca
AlissonDn <108025321+Dhytm@users.noreply.github.com>
2023-03-15 22:03:25 -0300

    --update models and interface
    updated models and interface

--- Classificador-de-Frutas-Legumes-e-Verduras/deploy/hortalica_app.py
+++ Classificador-de-Frutas-Legumes-e-Verduras/deploy/hortalica_app.py
@@ -9,14 +9,15 @@ import os
 import sklearn
 from unidecode import unidecode
 
-
-print(sklearn.__version__)
+st.set_page_config(page_title="Horticultural Classifier")
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
 def processador_de_texto(data):
     data = unidecode(data)
+    data = data.replace("-", " ")
+    data = data.replace("  ", " ")
     data = re.sub('[^\w\s]', '', str(data.lower()))
     data = data.replace("\r", " ")
     data = data.replace("\n", " ")
@@ -44,70 +45,82 @@ with open(f"{dir_path}/hortalicia_model.pkl", "rb") as f:
 h_classes = {0.0: "Fruta", 1.0: "Legume", 2.0: "Verdura", }
 
 # Título da página
-st.title("Bem-vindo ao meu Classificador de Frutas, Legumes e Verduras")
+st.title("My Fruit and Vegetable Classifier")
 
 # Create a text element and let the reader know the data is loading.
-data_load_state = st.text(
-    'Este é um projeto de classificação de frutas, legumes e verduras usando machine learning.' +
-    '\nO tipo de modelo que escolhi para essa tarefa é um modelo de Árvore de Decisão' +
-    ', especificamente o Random Forest Classifier.\n' +
-    'Forneça o nome de uma fruta, legume ou verdura e veja o resultado da classificação em tempo real!')
-
-data_load_state = st.text('Aviso: Embora eu tenha criado com muito cuidado este projeto, e o modelo por\n' +
-                          'trás dele esteja performando muito bem, algumas das classificações sugeridas pelo\n' +
-                          'modelo podem estar incorretas, portanto, atente-se a isso!')
-
+data_load_state = st.text('Este é um projeto de classificação de frutas, legumes e verduras que usa\n' +
+                          'machine learning. Escolhi um modelo de Árvore de Decisão para essa tarefa,\n' +
+                          'especificamente o Random Forest Classifier.\n' +
+                          '\n'
+                          'Forneça o nome de uma fruta, legume ou verdura e veja o resultado da\n' +
+                          'classificação em tempo real!')
 # Input do usuário
-input_value = st.text_input("Digite o nome de uma fruta, legume ou verdura:")
+with st.form(key='my_form'):
+    # st.text("Digite ou selecione algumas das opções")
+
+    input_value = st.text_input("Digite o nome de uma fruta, legume ou verdura",
+                                placeholder="Digite uma fruta, legume ou verdura e clique em Executar modelo")
+    choice = st.selectbox("Ou experimente alguma das opções a seguir",
+                          ["Selecionar", "Lucuma", "Escorcioneira", "Caqui",
+                           "Linhaça", "Cacau", "Cajá-manga", "Tarumã", "Sálvia",
+                           "Rutabaga", "Wasabi (folha)", "Pinhão", "Bredo",
+                           "Shissô", "Picão", "Gobô", "Kohlrabi", "Urtiga"],
+                          help="Selecione uma opção e clique em Executar modelo")
+    submit_button = st.form_submit_button(label='Executar modelo')
+
 
 # Mostra o resultado para o usuário
-if input_value:
-    st.write("Você digitou:", input_value)
+if input_value or choice != "Selecionar":
 
-# Adicionando um botão para o usuário clicar
-if st.button("Executar modelo"):
-    # Aqui você pode colocar o código para fazer a classificação usando o input do usuário
+    if choice != "Selecionar" and input_value == "":
+        input_value = choice
 
-    classify_vector = classify_new(
-        processador_de_texto(input_value), h_vectorizer)
+    # Adicionando um botão para o usuário clicar
+    # if st.button("Executar modelo"):
+    if submit_button:
+        # Aqui você pode colocar o código para fazer a classificação usando o input do usuário
 
-    # !!!
-    input_validation = True
+        classify_vector = classify_new(
+            processador_de_texto(input_value), h_vectorizer)
 
-    if not str(classify_vector):
-        input_validation = False
+        # !!!
+        input_validation = True
 
-    # !!!
-    if input_validation:
-        proba = model.predict_proba(classify_vector)
-        classification = model.predict(classify_vector)
+        if not str(classify_vector):
+            input_validation = False
 
-        result = f"'{input_value}' é classificado como '{h_classes[classification[0]]}'!!!"
-        st.success(result)
+        # !!!
+        if input_validation:
+            proba = model.predict_proba(classify_vector)
+            classification = model.predict(classify_vector)
 
-    else:
-        error_r = (
-            f"""
-        Desculpe-me, mas o pedido para '{input_value}' foi negado!\n
-        Pode ser que tenha ocorrido alguma falha na digitação ou o termo pode não se enquadrar nas categorias permitidas!
-        """
-        )
+            result = f"'{input_value}' é classificado como '{h_classes[classification[0]]}'!!!"
+            st.success(result)
 
-        st.error(error_r)
-        result = (
+        else:
+            error_r = (
+                f"""
+            Desculpe-me, mas o pedido para '{input_value}' foi negado!\n
+            Pode ser que tenha ocorrido alguma falha na digitação ou o termo pode não se enquadrar nas categorias permitidas!
             """
-            Caso acredite que tenha ocorrido um erro durante sua experiência de uso,
-            por favor, reporte imediatamente ao meu desenvolvedor.
-            Para isso, clique no botão "Reportar um bug" que pode ser acessado
-            nas opções do menu localizado na lateral superior direita da tela.\n
-            Obrigado por sua colaboração!
-            """
-        )
-        st.info(result)
+            )
+
+            st.error(error_r)
+            result = (
+                """
+                Caso acredite que tenha ocorrido um erro durante sua experiência de uso,
+                por favor, reporte imediatamente ao meu desenvolvedor.
+                Para isso, clique no botão "Reportar um bug" que pode ser acessado
+                nas opções do menu localizado na lateral superior direita da tela.\n
+                Obrigado por sua colaboração!
+                """
+            )
+            st.info(result)
+elif submit_button:
+    st.error("Digite alguma coisa ou selecione um item!")
 
 data_load_state = st.text(
     'Projeto de machine learning criado por Alisson Silva\n' +
-    'Meu GitHub: https://github.com/Dhytm\n' +
     'Licença: https://github.com/Dhytm/Projects/blob/main/LICENSE\n')
 
 data_load_state = st.text(
